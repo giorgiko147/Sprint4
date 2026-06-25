64f133f3e79182f9c813c8235621204d24387e96
Giorgi <giorgi@Giorgi.Machine.20>
2026-06-25 09:54:49 -0700

    Added a streamlit dashboard

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('/home/giorgi/Sprint4/vehicles_us.csv')
+# Header
+st.header('Vehicle Advertisement Dashboard')
+
+# Histogram
+if st.checkbox('Show price distribution'):
+    fig = px.histogram(
+        df,
+        x='price',
+        title='Distribution of Vehicle Prices'
+    )
+    st.plotly_chart(fig)
+
+# Scatter plot
+if st.checkbox('Show price vs mileage'):
+    fig = px.scatter(
+        df,
+        x='odometer',
+        y='price',
+        title='Price vs Mileage'
+    )
+    st.plotly_chart(fig)
