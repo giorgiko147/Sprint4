8715d32a2c42708f1e7f34d5151f948045a93d8c
Giorgi <giorgi@Giorgi.Machine.20>
2026-06-25 10:51:21 -0700

    changed read.csv command to correct file pathway

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv('/home/giorgi/Sprint4/vehicles_us.csv')
+df = pd.read_csv('vehicles_us.csv')
 # Header
 st.header('Vehicle Advertisement Dashboard')
 
