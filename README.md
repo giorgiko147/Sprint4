060ff000a89c6f1bfae26ffa88e1c44a43ddda20
Giorgi <giorgi@Giorgi.Machine.20>
2026-06-26 09:51:53 -0700

    Changed the app.py to include a filter and added a more descriptive READ.ME file

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# Sprint4
+#Sprint 4
+
+Vehicle Advertisement Dashboard
+
+Project Description:
+
+This is a web dashboard built with Streamlit. It analyzes a used vehicle advertisements dataset. The graphs allow the users to visually see the relationships between vehicle price, mileage, and condition.
+
+The application is deployed on Render.
+
+Project Objective:
+
+The goal is to practice software development tools and workflows such as:
+
+-Creating a Python project environment
+-Using Git and GitHub
+-Using EDA on a CSV dataset
+-Building a web application with Streamlit
+-Creating visualizations
+-Deploying a web application to Render
+
+Link to The Render Website:
+
 https://sprint4-2dru.onrender.com/
 
+Link To the GitHub Repository:
+
+https://github.com/giorgiko147/Sprint4
--- app.py
+++ app.py
@@ -1,26 +1,48 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 import plotly.express as px
 
 df = pd.read_csv('vehicles_us.csv')
-# Header
+
 st.header('Vehicle Advertisement Dashboard')
 
-# Histogram
-if st.checkbox('Show price distribution'):
-    fig = px.histogram(
-        df,
-        x='price',
-        title='Distribution of Vehicle Prices'
-    )
-    st.plotly_chart(fig)
+st.write(
+    'This dashboard explores used vehicle advertisement data. '
+    'Select which vehicles are included in the graphs below.'
+)
 
-# Scatter plot
-if st.checkbox('Show price vs mileage'):
-    fig = px.scatter(
-        df,
-        x='odometer',
-        y='price',
-        title='Price vs Mileage'
-    )
-    st.plotly_chart(fig)
+# Creating the filter
+#Select a vehicle condition
+
+condition_options = df['condition'].dropna().unique()
+
+selected_conditions = st.multiselect(
+    'Select Vehicle Condition',
+    options=condition_options,
+    default=condition_options
+)
+
+# Apply to the df
+filtered_df = df[df['condition'].isin(selected_conditions)]
+
+st.write(f'Number of vehicles selected: {len(filtered_df)}')
+
+# Histogram 
+fig_hist = px.histogram(
+    filtered_df,
+    x='price',
+    title='Distribution of Vehicle Prices'
+)
+
+st.plotly_chart(fig_hist)
+
+# Scatter plot 
+fig_scatter = px.scatter(
+    filtered_df,
+    x='odometer',
+    y='price',
+    color='condition',
+    title='Vehicle Price vs Mileage by Condition'
+)
+
+st.plotly_chart(fig_scatter)
